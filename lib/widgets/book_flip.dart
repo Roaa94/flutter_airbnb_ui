760ce09242ff122cb18e807353dada491b22b84a
Roaa94 <roaa94khaddam@gmail.com>
2023-05-23 01:13:25 +0300

    mak the flip animation work with animation controller

--- lib/widgets/book_flip.dart
+++ lib/widgets/book_flip.dart
@@ -21,67 +21,85 @@ class BookFlip extends StatefulWidget {
 class _BookFlipState extends State<BookFlip>
     with SingleTickerProviderStateMixin {
   late final AnimationController _animationController;
-  double _sliderValue = 0;
+  late final Animation<double> _animation;
 
   @override
   void initState() {
+    super.initState();
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 1000),
+      duration: const Duration(milliseconds: 500),
     );
-    super.initState();
+    _animation = Tween<double>(begin: 1, end: 0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FractionalTranslation(
-          translation: Offset(-0.5 * _sliderValue, 0),
-          child: Container(
-            constraints: const BoxConstraints(maxHeight: 220),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              textDirection: TextDirection.rtl,
-              children: [
-                Expanded(
-                  child: BookBack(widget.listing),
-                ),
-                Expanded(
-                  child: Transform(
-                    transform: Matrix4.identity()
-                      ..setEntry(3, 2, 0.001)
-                      ..rotateY(-pi * _sliderValue),
-                    alignment: Alignment.centerRight,
-                    child: Stack(
-                      children: [
-                        _sliderValue <= 0.5
-                            ? Positioned.fill(
-                                child: BookCoverBack(widget.listing),
-                              )
-                            : Positioned.fill(
-                                child: Transform(
-                                  transform: Matrix4.identity()
-                                    ..setEntry(3, 2, 0.001)
-                                    ..rotateY(-pi),
-                                  alignment: Alignment.center,
-                                  child: BookCoverFront(widget.listing),
-                                ),
-                              ),
-                      ],
+        AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            return FractionalTranslation(
+              translation: Offset(-0.5 * _animation.value, 0),
+              child: Container(
+                constraints: const BoxConstraints(maxHeight: 220),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  textDirection: TextDirection.rtl,
+                  children: [
+                    Expanded(
+                      child: BookBack(widget.listing),
                     ),
-                  ),
+                    Expanded(
+                      child: Transform(
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, 0.001)
+                          ..rotateY(-pi * _animation.value),
+                        alignment: Alignment.centerRight,
+                        child: Stack(
+                          children: [
+                            _animation.value <= 0.5
+                                ? Positioned.fill(
+                                    child: BookCoverBack(widget.listing),
+                                  )
+                                : Positioned.fill(
+                                    child: Transform(
+                                      transform: Matrix4.identity()
+                                        ..setEntry(3, 2, 0.001)
+                                        ..rotateY(-pi),
+                                      alignment: Alignment.center,
+                                      child: BookCoverFront(widget.listing),
+                                    ),
+                                  ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
-        Slider(
-          min: 0,
-          max: 1,
-          value: _sliderValue,
-          onChanged: (value) => setState(() => _sliderValue = value),
-        )
+        ElevatedButton(
+          onPressed: () {
+            if (_animationController.isCompleted) {
+              _animationController.reverse();
+            } else {
+              _animationController.forward();
+            }
+          },
+          child: const Text('Run'),
+        ),
       ],
     );
   }
