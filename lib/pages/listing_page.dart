5a66e01cb89df4d8993b88b489313890c78655e2
Roaa94 <roaa94khaddam@gmail.com>
2023-05-25 20:55:01 +0300

    dismissing listing page as if it's a bottom sheet 🤭

--- lib/pages/listing_page.dart
+++ lib/pages/listing_page.dart
@@ -14,85 +14,105 @@ class ListingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 200),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.grey.shade100,
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification scroll) {
+          if (scroll is OverscrollNotification &&
+              scroll.dragDetails != null &&
+              scroll.dragDetails!.delta.dy > 10) {
+            Navigator.of(context).pop();
+            return false;
+          }
+          return true;
+        },
+        child: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+          child: Column(
+            children: [
+              const SizedBox(height: 200),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Container(
+                        width: 50,
+                        height: 4,
+                        margin: const EdgeInsets.only(top: 15),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade400,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Hero(
+                        tag: 'listing_hero_${listing.id}',
+                        flightShuttleBuilder: (
+                          BuildContext flightContext,
+                          Animation<double> animation,
+                          HeroFlightDirection flightDirection,
+                          BuildContext fromHeroContext,
+                          BuildContext toHeroContext,
+                        ) {
+                          return BookFlip(
+                            listing,
+                            parentAnimation: animation,
+                          );
+                        },
+                        child: BookFlip(
+                          listing,
+                          initialFlipProgress: 1,
+                        ),
+                      ),
+                    ),
+                    ...List.generate(
+                      listing.infoItems.length,
+                      (index) => Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 5),
+                        child: Row(
+                          children: [
+                            Icon(
+                              listing.infoItems[index].icon,
+                              size: 30,
+                            ),
+                            const SizedBox(width: 10),
+                            Expanded(
+                              child: Text(
+                                listing.infoItems[index].text,
+                                style: const TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                      child: Text(
+                        listing.landlordDescription,
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                  ],
                 ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  IconButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    icon: const Icon(Icons.close),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Hero(
-                      tag: 'listing_hero_${listing.id}',
-                      flightShuttleBuilder: (
-                        BuildContext flightContext,
-                        Animation<double> animation,
-                        HeroFlightDirection flightDirection,
-                        BuildContext fromHeroContext,
-                        BuildContext toHeroContext,
-                      ) {
-                        return BookFlip(
-                          listing,
-                          parentAnimation: animation,
-                        );
-                      },
-                      child: BookFlip(
-                        listing,
-                        initialFlipProgress: 1,
-                      ),
-                    ),
-                  ),
-                  ...List.generate(
-                    listing.infoItems.length,
-                    (index) => Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 5),
-                      child: Row(
-                        children: [
-                          Icon(
-                            listing.infoItems[index].icon,
-                            size: 30,
-                          ),
-                          const SizedBox(width: 10),
-                          Expanded(
-                            child: Text(
-                              listing.infoItems[index].text,
-                              style: const TextStyle(fontSize: 18),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 10,
-                    ),
-                    child: Text(
-                      listing.landlordDescription,
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
